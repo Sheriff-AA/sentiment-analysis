812a93a7e4e66589757c057263f3729ace1fccfc
Sherif <sherifatitebi@gmail.com>
2025-01-25 17:27:43 +0000

    installing tailwind

--- sentiment/forms.py
+++ sentiment/forms.py
@@ -3,9 +3,12 @@ from django import forms
 
 class SentimentTextForm(forms.Form):
     text = forms.CharField(
-        widget=forms.Textarea(attrs={'placeholder': 'Enter your text here', 'rows': 5, 'cols': 40}),
+        widget=forms.Textarea(
+            attrs={'placeholder': 'Enter your text here', 
+                   'rows': 5, 'cols': 40, 
+                   'class':"block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"}),
         label="Text",
-        required=True
+        required=True,
     )
     task = forms.ChoiceField(
         choices=[
